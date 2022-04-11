0eca1f490c34081b46b8774d88ae14080b1dd6a2
Alejandra <alejandra.sanchez@definityfirst.com>
2022-04-11 14:21:59 -0500

    get error

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ export const validateKey = (req: Request, Response: Response, next: NextFunction
         
         if(apiKey === 'g4bWl2eSKX3QQ4tgR0yZ31TzwpZcO796akpF0Zuy'){
             return next();
+        }else{
+            console.log('error');
         }
        
       } catch (err) {
